36a61a215bb5388987f53de5fc641740a3639cc8
Raffael Krebs <raffael.krebs@students.unibe.ch>
2012-03-28 18:21:32 +0200

    added a config.assets.precompile direction to include vendor javascript

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,8 @@ Badi2010::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  config.assets.precompile += %w( xstooltip.js )
 
   # Generate digests for assets URLs
   config.assets.digest = true
