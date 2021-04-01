37da66d5351a54e78f985b8c3b2f59020e8be98f
Raffael Krebs <raffael.krebs@protonmail.ch>
2021-04-01 10:57:21 +0200

    Fix timezone issues with shiftinfo times
    Setting the time zone in the initializer didn't seem to have had any effect. Strange.
    So when we set the time zone in the application.rb starting with Rails 6.1, all the times changed. Gah.
    Furthermore, since the time in the database were not "naïve", daylight savings time would have an ill effect. (All the more since the one-hour shift would occur right during the planning phase for the current year.)
    So back to UTC. Bern would be nicer but meh.

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,12 @@ module Badi2010
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Bern"
     config.autoload_paths += Dir[Rails.root.join("lib")]
     config.eager_load_paths += Dir[Rails.root.join("lib")]
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # NB: We ran into problems when setting this to "Bern" because of daylight saving time and auto-conversion.
+    config.time_zone = "UTC"
   end
 end
