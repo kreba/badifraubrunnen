f82806b3a8edfbb5bb2ae4cbc963d58339241106
Raffael Krebs <raffael.krebs@protonmail.ch>
2021-07-25 17:00:24 +0200

    Allow the webmaster to add people

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,12 @@ class ApplicationController < ActionController::Base
     # I18n.locale = current_user.language if logged_in?
   end
 
-  def restrict_access( role_name )
+  def restrict_access( role_name_or_array )
     saison_if_given = Saison.find_by name: params[:saison_name]
-    unless current_person.has_role? role_name, saison_if_given
-      role_descr = t('role.' + (saison_if_given ? params[:saison_name] + role_name.capitalize : role_name))
+    unless current_person.has_role? role_name_or_array, saison_if_given
+      role_descr = [*role_name_or_array].map do |role_name|
+        t('role.' + (saison_if_given ? params[:saison_name] + role_name.capitalize : role_name))
+      end.join(' ' + t('or') + ' ')
       flash[:error] = t('application.access_restricted', role: role_descr)
       redirect_back_or_default( home_path ) and return false
     end
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,8 +1,8 @@
 class PeopleController < ApplicationController
 
-  # Do not allow anyone to subscribe by themselfes
+  # Do not allow anyone to subscribe by themselves
   #skip_before_action :login_required, only: [:new, :create]
-  before_action except: [:edit, :find_location, :index, :update] do |c| c.restrict_access 'admin' end
+  before_action only: [:new, :create, :destroy] do |c| c.restrict_access ['admin', 'webmaster'] end
 
   # GET /people
   def index
